61ea76324cd387e73891b8eed3beb6688de72b26
Dr Nic Williams <drnicwilliams@gmail.com>
2017-03-29 09:04:43 +1000

    no extra release inputs required

--- ci/repipe.sh
+++ ci/repipe.sh
@@ -6,7 +6,7 @@ cd $DIR
 set -e
 
 echo fetching credentials
-spruce --concourse merge pipeline.yml creds.yml > pipeline-deploy.yml
+spruce merge pipeline.yml creds.yml > pipeline-deploy.yml
 
 cd $DIR/..
 fly -t vsphere sp -p $(basename $(pwd)) -c ci/pipeline-deploy.yml
