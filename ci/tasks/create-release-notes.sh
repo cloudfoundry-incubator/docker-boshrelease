8956bf6313368b3b34176747aada4546ba72ae9d
Dr Nic Williams <drnicwilliams@gmail.com>
2017-03-29 11:04:14 +1000

    include sample manifest/op patch file snippets

--- ci/tasks/create-release-notes.sh
+++ ci/tasks/create-release-notes.sh
@@ -4,12 +4,37 @@ set -e
 
 NOTES=$PWD/release-notes
 
-version=$(cat version/number)
+release_version=$(cat version/number)
+release_sha1=$(sha1sum final-release-tarball/*.tgz | head -n1 | awk '{print $1}')
 
-echo v${version} > $NOTES/release-name
+echo v${release_version} > $NOTES/release-name
 
 cat > $NOTES/notes.md <<EOF
 ## Improvements
 
 TODO
+
+## Deployment
+
+Deployment manifest snippet:
+
+\`\`\`yaml
+releases:
+- name: docker
+  version: ${release_version}
+  url: https://github.com/cloudfoundry-community/docker-boshrelease/releases/download/v${release_version}/docker-${release_version}.tgz
+  sha1: ${release_sha1}
+\`\`\`
+
+Or \`bosh2\` operator patch file:
+
+\`\`\`yaml
+- type: replace
+  path: /releases/name=docker
+  value:
+    version: ${release_version}
+    url: https://github.com/cloudfoundry-community/docker-boshrelease/releases/download/v${release_version}/docker-${release_version}.tgz
+    sha1: ${release_sha1}
+\`\`\`
+
 EOF
