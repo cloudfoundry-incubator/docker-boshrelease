6628005aaecc6b3641157122cbb56947412d1e50
Dr Nic Williams <drnicwilliams@gmail.com>
2017-04-02 17:16:21 +1000

    ignore upload-release errors for the moment

--- ci/tasks/bosh2-upload-release.sh
+++ ci/tasks/bosh2-upload-release.sh
@@ -5,4 +5,4 @@ export BOSH_CA_CERT=${BOSH_CA_CERT:?required}
 export BOSH_CLIENT=${BOSH_CLIENT:?required}
 export BOSH_CLIENT_SECRET=${BOSH_CLIENT_SECRET:?required}
 
-bosh2 upload-release candidate-release/*.tgz
+bosh2 upload-release candidate-release/*.tgz || echo "error; but perhaps ok"
