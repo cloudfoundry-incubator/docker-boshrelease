4ff479e02693b57468f7c2f6b881bdf191fd6e70
Dr Nic Williams <drnicwilliams@gmail.com>
2017-10-31 07:32:05 +1000

    moving to pipeline-templates

--- ci/tasks/bosh2-upload-release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export BOSH_ENVIRONMENT=${BOSH_ENVIRONMENT:?required}
-export BOSH_CA_CERT=${BOSH_CA_CERT:?required}
-export BOSH_CLIENT=${BOSH_CLIENT:?required}
-export BOSH_CLIENT_SECRET=${BOSH_CLIENT_SECRET:?required}
-
-bosh2 upload-release candidate-release/*.tgz || echo "error; but perhaps ok"
